269a1892c7c422ccda01e0163052cc15e593d544
Michael Hicks <michaelhicks@saiplyus.com>
2022-05-05 01:13:39 -0400

    5/4/2022

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 (function () {
-  let apples = [
+  let fruits = [
     {
       src: "./images/apple-braeburn.png",
       width: "200",
       height: "200",
       alt: "Braeburn Apple",
       type: "Braeburn Apple",
+      fruit: "Apple",
     },
     {
       src: "./images/apple-cortland.png",
@@ -13,6 +14,7 @@
       height: "200",
       alt: "Cortland Apple",
       type: "Cortland Apple",
+      fruit: "Apple",
     },
     {
       src: "./images/apple-gala.png",
@@ -20,6 +22,7 @@
       height: "200",
       alt: "Gala Apple",
       type: "Gala Apple",
+      fruit: "Apple",
     },
     {
       src: "./images/apple-ginger-gold.png",
@@ -27,6 +30,7 @@
       height: "200",
       alt: "Ginger Gold Apple",
       type: "Ginger Gold Apple",
+      fruit: "Apple",
     },
     {
       src: "./images/apple-golden-delicious.png",
@@ -34,6 +38,7 @@
       height: "200",
       alt: "Golden Delicious Apple",
       type: "Golden Delicious Apple",
+      fruit: "Apple",
     },
     {
       src: "./images/apple-granny-smith.png",
@@ -41,6 +46,7 @@
       height: "200",
       alt: "Granny Smith Apple",
       type: "Granny Smith Apple",
+      fruit: "Apple",
     },
     {
       src: "./images/apple-honeycrisp.png",
@@ -48,6 +54,7 @@
       height: "200",
       alt: "Honeycrisp Apple",
       type: "Honeycrisp Apple",
+      fruit: "Apple",
     },
     {
       src: "./images/apple-jonagold.png",
@@ -55,6 +62,7 @@
       height: "200",
       alt: "Jonagold Apple",
       type: "Jonagold Apple",
+      fruit: "Apple",
     },
     {
       src: "./images/apple-jonathan.png",
@@ -62,6 +70,7 @@
       height: "200",
       alt: "Jonathan Apple",
       type: "Jonathan Apple",
+      fruit: "Apple",
     },
     {
       src: "./images/apple-mcintosh.png",
@@ -69,6 +78,7 @@
       height: "200",
       alt: "Mcintosh Apple",
       type: "Mcintosh Apple",
+      fruit: "Apple",
     },
     {
       src: "./images/apple-pacific-rose.png",
@@ -76,6 +86,7 @@
       height: "200",
       alt: "Pacific Rose Apple",
       type: "Pacific Rose Apple",
+      fruit: "Apple",
     },
     {
       src: "./images/apple-paula-red.png",
@@ -83,6 +94,7 @@
       height: "200",
       alt: "Paula Red Apple",
       type: "Paula Red Apple",
+      fruit: "Apple",
     },
     {
       src: "./images/apple-red-delicious.png",
@@ -90,6 +102,7 @@
       height: "200",
       alt: "Red Delicious Apple",
       type: "Red Delicious Apple",
+      fruit: "Apple",
     },
     {
       src: "./images/apple-wealthy.png",
@@ -97,15 +110,15 @@
       height: "200",
       alt: "Wealthy Apple",
       type: "Wealthy Apple",
+      fruit: "Apple",
     },
-  ];
-  let oranges = [
     {
       src: "./images/orange-blood.png",
       width: "200",
       height: "200",
       alt: "Blood Orange",
       type: "Blood Orange",
+      fruit: "Orange",
     },
     {
       src: "./images/orange-cara-cara.png",
@@ -113,6 +126,7 @@
       height: "200",
       alt: "Cara Cara Orange",
       type: "Cara Cara Orange",
+      fruit: "Orange",
     },
     {
       src: "./images/orange-clementine.png",
@@ -120,6 +134,7 @@
       height: "200",
       alt: "Clementine Orange",
       type: "Clementine Orange",
+      fruit: "Orange",
     },
     {
       src: "./images/orange-florida-grapefruit.png",
@@ -127,6 +142,7 @@
       height: "200",
       alt: "Florida Grapefruit",
       type: "Florida Grapefruit",
+      fruit: "Orange",
     },
     {
       src: "./images/orange-golden-nugget.png",
@@ -134,6 +150,7 @@
       height: "200",
       alt: "Golden Nugget Orange",
       type: "Golden Nugget Orange",
+      fruit: "Orange",
     },
     {
       src: "./images/orange-heirloom-navel.png",
@@ -141,6 +158,7 @@
       height: "200",
       alt: "Heirloom Navel Orange",
       type: "Heirloom Navel Orange",
+      fruit: "Orange",
     },
     {
       src: "./images/orange-mandarin.png",
@@ -148,6 +166,7 @@
       height: "200",
       alt: "Mandarin Orange",
       type: "Mandarin Orange",
+      fruit: "Orange",
     },
     {
       src: "./images/orange-murcott.png",
@@ -155,6 +174,7 @@
       height: "200",
       alt: "Murcott Mandarin",
       type: "Murcott Mandarin",
+      fruit: "Orange",
     },
     {
       src: "./images/orange-pink-grapefruit.png",
@@ -162,6 +182,7 @@
       height: "200",
       alt: "Pink Grapefruit",
       type: "Pink Grapefruit",
+      fruit: "Orange",
     },
     {
       src: "./images/orange-seville-sour.png",
@@ -169,6 +190,7 @@
       height: "200",
       alt: "Seville Sour Orange",
       type: "Seville Sour Orange",
+      fruit: "Orange",
     },
     {
       src: "./images/orange-tangerine.png",
@@ -176,32 +198,28 @@
       height: "200",
       alt: "Tangerine",
       type: "Tangerine",
+      fruit: "Orange",
     },
   ];
-  for (value of apples) {
-    let createImgTag = document.createElement("img");
-    let createFigureTag = document.createElement("figure");
-    let createFigureCaption = document.createElement("figcaption");
+
+  for (value of fruits) {
+    const createImgTag = document.createElement("img");
+    const createFigureTag = document.createElement("figure");
+    const createFigureCaption = document.createElement("figcaption");
     createImgTag.src = value.src;
     createImgTag.width = value.width;
     createImgTag.height = value.height;
     createImgTag.alt = value.alt;
     createFigureCaption.innerText = `${value.type}`;
-    document.getElementById("apples").append(createFigureTag);
-    createFigureTag.append(createImgTag);
-    createFigureTag.append(createFigureCaption);
-  }
-  for (value of oranges) {
-    let createImgTag = document.createElement("img");
-    let createFigureTag = document.createElement("figure");
-    let createFigureCaption = document.createElement("figcaption");
-    createImgTag.src = value.src;
-    createImgTag.width = value.width;
-    createImgTag.height = value.height;
-    createImgTag.alt = value.alt;
-    createFigureCaption.innerText = `${value.type}`;
-    document.getElementById("oranges").append(createFigureTag);
-    createFigureTag.append(createImgTag);
-    createFigureTag.append(createFigureCaption);
+
+    if (value.fruit === "Apple") {
+      document.getElementById("apples").append(createFigureTag);
+      createFigureTag.append(createImgTag);
+      createFigureTag.append(createFigureCaption);
+    } else if (value.fruit === "Orange") {
+      document.getElementById("oranges").append(createFigureTag);
+      createFigureTag.append(createImgTag);
+      createFigureTag.append(createFigureCaption);
+    }
   }
 })();
